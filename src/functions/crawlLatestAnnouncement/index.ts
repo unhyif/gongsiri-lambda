bb120242bf35d0f7e6705fbe6a33d32908db6902
unhyif <ang21icjh@gmail.com>
2024-04-27 13:48:47 +0900

    fix: Invoked function failed에 따라 메모리 증가

--- src/functions/crawlLatestAnnouncement/index.ts
+++ src/functions/crawlLatestAnnouncement/index.ts
@@ -1,7 +1,7 @@
 import { handlerPath } from '@libs/handler-resolver';
 
 export default {
-  memorySize: 256,
+  memorySize: 512,
   timeout: 420,
   handler: `${handlerPath(__dirname)}/handler.crawlLatestAnnouncement`,
   events: [{ schedule: 'cron(0 0-9 ? * MON-FRI *)' }],
